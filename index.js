3a2898171ba12a476a9503ff72d4ebb4fb7be981
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-08-21 17:32:17 -0300

    Added fade in and out

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let engine = null;
 let dome = null;
 let scene = null;
 let photoIndex = 0;
+let fadeIn = false;
+let fadeOut = false;
+const MAX_PHOTO_INDEX = 3;
+const FRAME_FADE_PERCENT = 0.01;
 
 async function createScene() {
     // This creates a basic Babylon Scene object (non-mesh)
@@ -31,16 +35,21 @@ async function createScene() {
         switch (e.key) {
             case "a":
                 photoIndex--
+                fadeOut = true
+                fadeIn = false
 
                 if (photoIndex < 0) {
-                    photoIndex = 3
+                    photoIndex = MAX_PHOTO_INDEX
                 }
+
                 break;
 
             case "d":
                 photoIndex++
+                fadeOut = true
+                fadeIn = false
 
-                if (photoIndex > 3) {
+                if (photoIndex > MAX_PHOTO_INDEX) {
                     photoIndex = 0
                 }
                 break;
@@ -49,22 +58,24 @@ async function createScene() {
                 console.log("UNKOWN COMMAND")
                 break;
         }
-
-        if (dome) {
-            dome.dispose();
-            dome = null;
-        }
-
-        dome = new BABYLON.PhotoDome(
-            "testdome",
-            `./textures/photo${photoIndex}.jpg`,
-            {
-                resolution: 32,
-                size: 1000
-            },
-            scene
-        );
     });
+}
+
+function replaceDome(scene) {
+    if (dome) {
+        dome.dispose();
+        dome = null;
+    }
+
+    return new BABYLON.PhotoDome(
+        "testdome",
+        `./textures/photo${photoIndex}.jpg`,
+        {
+            resolution: 32,
+            size: 1000
+        },
+        scene
+    );
 }
 
 // Resize
@@ -83,8 +94,25 @@ async function initFunction() {
     await createScene();
 
     engine.runRenderLoop(function () {
-        if (scene && scene.activeCamera)
+        if (scene && scene.activeCamera) {
+            if (fadeOut) {
+                dome.material.alpha = Math.max(0, dome.material.alpha - FRAME_FADE_PERCENT);
+                if (dome.material.alpha <= 0) {
+                    // fade out acabou! Fazer algo!
+                    dome = replaceDome(scene);
+                    dome.material.alpha = 0
+
+                    fadeOut = false
+                    fadeIn = true
+                }
+            } else if (fadeIn) {
+                dome.material.alpha = Math.min(1, dome.material.alpha + FRAME_FADE_PERCENT);
+                if (dome.material.alpha >= 1) {
+                    // fade in acabou! Fazer algo!
+                }
+            }
             scene.render();
+        }
     });
 }
 
